4e9df0d82de56f4d01d38aa36b96ed4f42b753d9
Pavithrangt <118878998+Pavithrangt@users.noreply.github.com>
2023-11-07 10:19:13 +0530

    Add files via upload

---
+++ calculator/script.js
@@ -0,0 +1,18 @@
+    
+   
+    function appendToResult(value) {
+        document.getElementById('result').value += value;
+    }
+
+    function clearResult() {
+        document.getElementById('result').value = '';
+    }
+
+    function calculateResult() {
+        try {
+            document.getElementById('result').value = eval(document.getElementById('result').value);
+        } catch (error) {
+            alert('Error in calculation!');
+            clearResult();
+        }
+    }
